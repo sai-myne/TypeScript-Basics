726009bac9b439f95d1653fe855be1fd5df0ce6d
sai-myne <ahlusir@gmail.com>
2020-08-08 12:54:32 +0800

    Enums

--- public/app.js
+++ public/app.js
@@ -22,22 +22,23 @@ form.addEventListener('submit', (e) => {
     }
     list.render(doc, type.value, 'end');
 });
-// GENERICS
-const addUID = (obj) => {
-    let uid = Math.floor(Math.random() * 100);
-    return Object.assign(Object.assign({}, obj), { uid });
-};
-let docOne = addUID({ name: 'yoshi', age: 40 });
-// let docTwo = addUID('hello');
-console.log(docOne.age);
+// ENUMS
+var ResourceType;
+(function (ResourceType) {
+    ResourceType[ResourceType["BOOK"] = 0] = "BOOK";
+    ResourceType[ResourceType["AUTHOR"] = 1] = "AUTHOR";
+    ResourceType[ResourceType["FILM"] = 2] = "FILM";
+    ResourceType[ResourceType["DIRECTOR"] = 3] = "DIRECTOR";
+    ResourceType[ResourceType["PERSON"] = 4] = "PERSON";
+})(ResourceType || (ResourceType = {}));
 const docThree = {
     uid: 1,
-    resourceName: 'person',
-    data: { name: 'shaun' }
+    resourceType: ResourceType.BOOK,
+    data: { title: 'name of the wind' }
 };
 const docFour = {
     uid: 2,
-    resourceName: 'shoppingList',
-    data: ['test', 'milk', 'toilet roll']
+    resourceType: ResourceType.PERSON,
+    data: { name: 'yoshi' }
 };
 console.log(docThree, docFour);
--- src/app.ts
+++ src/app.ts
@@ -30,35 +30,25 @@ form.addEventListener('submit', (e: Event) => {
     list.render(doc, type.value, 'end');
 });
 
-// GENERICS
+// ENUMS
+enum ResourceType { BOOK, AUTHOR, FILM, DIRECTOR, PERSON }
 
-const addUID = <T extends {name: string}>(obj: T) => {
-    let uid = Math.floor(Math.random() * 100);
-    return {...obj, uid};
-}
-
-let docOne = addUID({name: 'yoshi', age: 40});
-// let docTwo = addUID('hello');
-
-console.log(docOne.age);
-
-// with interfaces
 interface Resource<T> {
     uid: number;
-    resourceName: string;
+    resourceType: ResourceType;
     data: T;
 }
 
 const docThree: Resource<object> = {
     uid: 1,
-    resourceName: 'person',
-    data: { name: 'shaun'}
+    resourceType: ResourceType.BOOK,
+    data: { title: 'name of the wind'}
 }
 
-const docFour: Resource<string[]> = {
+const docFour: Resource<object> = {
     uid: 2,
-    resourceName: 'shoppingList',
-    data: ['test', 'milk', 'toilet roll']
+    resourceType: ResourceType.PERSON,
+    data: { name: 'yoshi' }
 } 
 
 console.log(docThree, docFour);
